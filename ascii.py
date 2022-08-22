a7b86bd19a4298522904826f860b44ec9ee53e05
Raffons <mattia.raffa@gmail.com>
2022-08-22 16:41:15 +0200

    Add files via upload

--- ascii.py
+++ ascii.py
@@ -1,15 +1,13 @@
 from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 
-
-def asciiart(in_f, SC, GCF, out_f, color='#1D1D1B', bgcolor='white'):
+def asciiart(in_f, SC, GCF, out_f, symb, lines='', color='#1D1D1B', bgcolor='white'):
     # The array of ascii symbols from white to black
-    #chars = np.asarray(list(' .?##'))
-    chars = np.asarray(list('  .:*/(?[-###'))
+    chars = np.asarray(list(symb))
 
     # Load the fonts and then get the the height and width of a typical symbol
     # You can use different fonts here
-    font = ImageFont.truetype("nova-imagerand-regular.ttf")
+    font = ImageFont.truetype("nova-imagerand-regular V2")
     letter_width = font.getsize("#")[0]
     letter_height = font.getsize("#")[0]
 
@@ -37,7 +35,6 @@ def asciiart(in_f, SC, GCF, out_f, color='#1D1D1B', bgcolor='white'):
 
     # Generate the ascii art symbols
     lines = ("\n".join(("".join(r) for r in chars[img.astype(int)]))).split("\n")
-    #print(lines)
 
     # Create an image object, set its width and height
     newImg_width = letter_width * widthByLetter
@@ -60,12 +57,35 @@ def asciiart(in_f, SC, GCF, out_f, color='#1D1D1B', bgcolor='white'):
     # Save the image file
     newImg.save(out_f)
 
-def asciistring():
+    f = open("myfile.svg", "w")
+    start = '<?xml version="1.0" encoding="utf-8"?><!-- nòva ASCIIart - Raffons)  --><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 595.28 841.89" style="enable-background:new 0 0 595.28 841.89;" xml:space="preserve"><style type="text/css">.st0{font-family:"nova-imagerand-regular";}.st1{font-size:12px;}</style> <text>'
+
+    #image = '<tspan x="0" y="12" class="st0 st1">                                             </tspan><tspan x="0" y="24" class="st0 st1">                                             </tspan><tspan x="0" y="36" class="st0 st1">                                             </tspan><tspan x="0" y="48" class="st0 st1">                /*.###?:-## *                </tspan><tspan x="0" y="60" class="st0 st1">             [?##############-?#/            </tspan><tspan x="0" y="72" class="st0 st1">           [###################-(            </tspan><tspan x="0" y="84" class="st0 st1">         *#######################[           </tspan><tspan x="0" y="96" class="st0 st1">         #####################-###:          </tspan><tspan x="0" y="108" class="st0 st1">       :[#########################-          </tspan><tspan x="0" y="120" class="st0 st1">      ..-###############--####-#####         </tspan><tspan x="0" y="132" class="st0 st1">        ###################-#-#######        </tspan><tspan x="0" y="144" class="st0 st1">       *#############-###?#-#########        </tspan><tspan x="0" y="156" class="st0 st1">      (-#####-#-##-##-[##?-#-#####--#        </tspan><tspan x="0" y="168" class="st0 st1">      ##-####[?(------[?[--[[[?--##--        </tspan><tspan x="0" y="180" class="st0 st1">      [#####?(/?(/(*?(*?(*:**://-##--:       </tspan><tspan x="0" y="192" class="st0 st1">       ####[(///**:.....:.. ..::[-##-.       </tspan><tspan x="0" y="204" class="st0 st1">      :####?//*/*:....       ..:(#[-#        </tspan><tspan x="0" y="216" class="st0 st1">      [###-?//**:.....        .:(-##         </tspan><tspan x="0" y="228" class="st0 st1">       ####?/*::::....         :(-#          </tspan><tspan x="0" y="240" class="st0 st1">        -##(/**::.. . .       .:?--          </tspan><tspan x="0" y="252" class="st0 st1">        -##///*::.            ..?-.          </tspan><tspan x="0" y="264" class="st0 st1">        ?##///??/:.        .....?-           </tspan><tspan x="0" y="276" class="st0 st1">         *#((-?:*([(.   (??/(/[.?*           </tspan><tspan x="0" y="288" class="st0 st1">          [/(*:(*:(*:.  .((** ..[.           </tspan><tspan x="0" y="300" class="st0 st1">          [(/*[?-[///.  :/?(/: .[.           </tspan><tspan x="0" y="312" class="st0 st1">          *?/*/::.**/:...:     .[.           </tspan><tspan x="0" y="324" class="st0 st1">          :[**:.::*//* ..     ..[            </tspan><tspan x="0" y="336" class="st0 st1">           #/*:.::*//:    ..   .-            </tspan><tspan x="0" y="348" class="st0 st1">           #/*::::://* .      .:[            </tspan><tspan x="0" y="360" class="st0 st1">           #(/::.:*//: ..     ./#            </tspan><tspan x="0" y="372" class="st0 st1">           ?[*::.:*//: .. ..   /-            </tspan><tspan x="0" y="384" class="st0 st1">           ([/*:.:*//   .:.   .[?            </tspan><tspan x="0" y="396" class="st0 st1">           (#?*:.:/[?:.:??/*/ *??            </tspan><tspan x="0" y="408" class="st0 st1">            #?/:*([[[??[[?*?/:/[             </tspan><tspan x="0" y="420" class="st0 st1">            #-[*/[[(/**:.:***((#             </tspan><tspan x="0" y="432" class="st0 st1">            (-[?//./(   /.  .??-#:           </tspan><tspan x="0" y="444" class="st0 st1">             ##?(:::*....   ./####           </tspan><tspan x="0" y="456" class="st0 st1">           -###-(::*::::.  ./-:--#######[    </tspan><tspan x="0" y="468" class="st0 st1">       #######(#[/:::*-*   :-*:#-##########- </tspan><tspan x="0" y="480" class="st0 st1">     *########(/#?*...:    ?[::#-########### </tspan><tspan x="0" y="492" class="st0 st1"> *############(/(#?:: ..:*(#.::--#-######### </tspan><tspan x="0" y="504" class="st0 st1">###############(/?#?(*./((-...*--########### </tspan><tspan x="0" y="516" class="st0 st1">###############///(##----#:...--############ </tspan><tspan x="0" y="528" class="st0 st1">################//**//:::....---############ </tspan><tspan x="0" y="540" class="st0 st1">#################[*::*/*.../---#-########### </tspan><tspan x="0" y="552" class="st0 st1">####################/::.*-----#-############ </tspan><tspan x="0" y="564" class="st0 st1">######################--#---#####-##########</tspan>'
+    image = str(lines)
+    n = 0
+
+    image = image[2:-2]
+
+    for j in range(0, (image.count("', '"))):
+        if n == 0:
+            image = image.replace("', '", '<tspan x="0" y="' + str(n) +'" class="st0 st1">', 1)
+            n = n+12
+        else:
+            image = image.replace("', '", '</tspan><tspan x="0" y="' + str(n) +'" class="st0 st1">', 1)
+            n = n+12
+
+    end = '</tspan></text></svg>'
+    f.write(start + image + end)
+    f.close()
+
+
+#def asciistring():
     # Generate the ascii art symbols
-    lines = ("\n".join(("".join(r) for r in chars[img.astype(int)]))).split("\n")
+    #lines = ("\n".join(("".join(r) for r in chars[img.astype(int)]))).split("\n")
+    #print(lines)
 
-def asciiwidth():
-    width = asciiart.newImg_width
+#def asciiwidth():
+    #width = asciiart.newImg_width
 
-def asciiheight():
-    height = asciiart.newImg_height
+#def asciiheight():
+    #height = asciiart.newImg_height
--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 import ascii
 
-# The function convert an image to ascii art
-# f: Input filename
-# SC: the horizontal pixel sampling rate. It should be between 0(exclusive) and 1(inclusive). The larger the number, the more details in the output.
-#   If you want the ascii art output be the same size as input, use ~ 1/ font size width.
-# GCF: >0. It's an image tuning factor. If GCF>1, the image will look brighter; if 0<GCF<1, the image will look darker.
-# out_f: output filename
-# color1, color2, bgcolor: follow W3C color naming https://www.w3.org/TR/css3-color/#svg-color
-#
-# Copyright 2017, Shanshan Wang, MIT license
+# The function convert an image to ascii art (png and svg)
+#basecode by 2017, Shanshan Wang, MIT license
 
 if __name__ == '__main__':
     inputf = "cvphoto.jpg"  # Input image file name
 
+    SC = 0.1  # pixel sampling rate in width (0-1] less details - more details
+    GCF = 1  # contrast adjustment (0-1-inf) darker-normal-brighter
 
-    SC = 0.1  # pixel sampling rate in width
-    GCF = 1  # contrast adjustment
+    color = '#1D1D1B' #gray
+    #color = '#ff3b2c' #red
+    #color = '#9225a5' #purple
+    #color = '#f8c537' #yellow
+    #color = '#0a3264' #blue
 
-    color = '#1D1D1B'
     bgcolor = 'white'
+    #bgcolor = '#ff3b2c' #red
+    #bgcolor = '#9225a5' #purple
+    #bgcolor = '#f8c537' #yellow
+    #bgcolor = '#0a3264' #blue
 
-    ascii.asciiart(inputf, SC, GCF, "results.png", color, bgcolor)
+    symbols = '  .:*/(?[-###'  # array of ascii symbols from white to black (spaces included)
+
+
+
+    ascii.asciiart(inputf, SC, GCF, "results.png", symbols,'' ,color, bgcolor)
+
